1100c56cf632b0b29f499dd8125d722d77ae5167
Talagozis <talagozis8@gmail.com>
2020-04-11 16:38:28 +0300

    Added web.config files. Adjusted .gitignore. Added logging.

--- Phoenix.Api/Program.cs
+++ Phoenix.Api/Program.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Talagozis.AspNetCore.Services.Logger;
+using Talagozis.AspNetCore.Services.Logger.ColoredConsole;
+using Talagozis.AspNetCore.Services.Logger.File;
 
 namespace Phoenix.Api
 {
@@ -21,6 +25,62 @@ namespace Phoenix.Api
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureKestrel((context, options) => options.AddServerHeader = false);
+                })
+                .ConfigureServices(serviceCollection => { serviceCollection.AddLoggerBackgroundService(); })
+                .ConfigureLogging(loggingBuilder =>
+                {
+                    loggingBuilder.SetMinimumLevel(LogLevel.Trace);
+
+                    loggingBuilder.AddFile(a =>
+                    {
+                        a.folderPath = Path.Combine(Directory.GetCurrentDirectory(), "../logs/");
+                        a.Add(new FileLoggerConfiguration
+                        {
+                            logLevel = LogLevel.Warning
+                        });
+                        a.Add(new FileLoggerConfiguration
+                        {
+                            logLevel = LogLevel.Error
+                        });
+                        a.Add(new FileLoggerConfiguration
+                        {
+                            logLevel = LogLevel.Critical
+                        });
+                    });
+#if DEBUG
+                    loggingBuilder.AddColoredConsole(a =>
+                    {
+                        a.Add(new ColoredConsoleLoggerConfiguration
+                        {
+                            color = ConsoleColor.Red,
+                            logLevel = LogLevel.Error,
+                        });
+                        a.Add(new ColoredConsoleLoggerConfiguration
+                        {
+                            color = ConsoleColor.Yellow,
+                            logLevel = LogLevel.Warning,
+                        });
+                        a.Add(new ColoredConsoleLoggerConfiguration
+                        {
+                            namespacePrefix = "Phoenix",
+                            color = ConsoleColor.Green,
+                            logLevel = LogLevel.Information,
+                        });
+                        a.Add(new ColoredConsoleLoggerConfiguration
+                        {
+                            namespacePrefix = "Phoenix",
+                            color = ConsoleColor.DarkCyan,
+                            logLevel = LogLevel.Debug,
+                        });
+                        a.Add(new ColoredConsoleLoggerConfiguration
+                        {
+                            namespacePrefix = "Phoenix",
+                            color = ConsoleColor.White,
+                            logLevel = LogLevel.Trace,
+                        });
+                    });
+#endif
                 });
     }
 }
