204bd439ef12b0ea6d9e45adab6791373ce13923
Théophile Spyrou <alfatheo21@gmail.com>
2020-04-11 18:23:51 +0300

    PhoenixDB connection string in Bot

--- Phoenix.Bot/Startup.cs
+++ Phoenix.Bot/Startup.cs
@@ -20,13 +20,13 @@ namespace Phoenix.Bot
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public static void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
             // Create the Bot Framework Adapter with error handling enabled.
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
 
             // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
-            services.AddSingleton<IStorage, MemoryStorage>();
+            services.AddSingleton<IStorage, MemoryStorage>(); //(new EntityFrameworkStorage(Configuration.GetConnectionString("PhoenixDB")));
 
             // Create the User state. (Used in this bot's Dialog implementation.)
             services.AddSingleton<UserState>();
