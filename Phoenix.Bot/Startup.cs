5fd7086b43853d81449d6ef787aab3e6638a0f9a
Théophile Spyrou <alfatheo21@gmail.com>
2020-04-13 20:08:37 +0300

    Update Startup file (add DB and identity)

--- Phoenix.Bot/Startup.cs
+++ Phoenix.Bot/Startup.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Phoenix.Bot.Bots;
 using Phoenix.Bot.Dialogs;
+using Phoenix.DataHandle.Models;
 
 namespace Phoenix.Bot
 {
@@ -42,7 +45,12 @@ namespace Phoenix.Bot
             services.AddApplicationInsightsTelemetry();
 
             services.AddControllers();
-            //services.AddRazorPages();
+
+            services.AddHttpsRedirection(options => options.HttpsPort = 443);
+
+            services.AddDbContext<PhoenixContext>(options => options.UseSqlServer(Configuration.GetConnectionString("PhoenixConnection")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<PhoenixContext>().AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,10 +66,11 @@ namespace Phoenix.Bot
             app.UseWebSockets();
             app.UseRouting();
 
+            app.UseAuthentication();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                //endpoints.MapRazorPages();
             });
         }
     }
