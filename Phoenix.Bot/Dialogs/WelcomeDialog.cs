c78ea955f5916de002fd14e39cbc3db290de5db0
Théophile Spyrou <alfatheo21@gmail.com>
2020-04-13 20:07:18 +0300

    Add Welcome Dialog

---
+++ Phoenix.Bot/Dialogs/WelcomeDialog.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phoenix.Bot.Dialogs
+{
+    public class WelcomeDialog : ComponentDialog
+    {
+        public WelcomeDialog()
+            : base(nameof(WelcomeDialog))
+        {
+            AddDialog(new WaterfallDialog(nameof(WelcomeDialog) + "_" + nameof(WaterfallDialog),
+                new WaterfallStep[]
+                {
+                    
+                }));
+
+            InitialDialogId = nameof(WelcomeDialog) + "_" + nameof(WaterfallDialog);
+        }
+    }
+}
