788d76e51040d9d2f95ce10e7d5f5f05bd8e0aa3
Théophile Spyrou <alfatheo21@gmail.com>
2020-05-23 23:06:09 +0300

    Implement Greek names for LectureStatus cases

--- Phoenix.DataHandle/Main/LectureStatus.cs
+++ Phoenix.DataHandle/Main/LectureStatus.cs
@@ -6,4 +6,22 @@
         Scheduled,
         Cancelled
     }
+
+    public static class LectureStatusExtensions
+    {
+        public static string ToGreekString(this LectureStatus ls)
+        {
+            switch (ls)
+            {
+                case LectureStatus.Unknown:
+                    return "Άγνωστη";
+                case LectureStatus.Scheduled:
+                    return "Κανονικά";
+                case LectureStatus.Cancelled:
+                    return "Ακυρώθηκε";
+            }
+
+            return ls.ToString();
+        }
+    }
 }
