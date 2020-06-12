95b16754555168290f3d0e7f2b8d105e28d94d54
Talagozis <talagozis8@gmail.com>
2020-06-12 18:33:19 +0300

    Added Phoenix.Scheduler project.

---
+++ Phoenix.Scheduler/App_Plugins/Services/LectureService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using JustScheduler;
+using Microsoft.Extensions.Logging;
+
+namespace Phoenix.Scheduler.App_Plugins.Services
+{
+    public class LectureService : IJob
+    {
+        private readonly ILogger _logger;
+
+        public LectureService(ILogger<LectureService> logger)
+        {
+            this._logger = logger;
+        }
+
+        public Task Run(CancellationToken cancellationToken)
+        {
+            this._logger.LogTrace("Test");
+
+            return Task.CompletedTask;
+        }
+    }
+}
