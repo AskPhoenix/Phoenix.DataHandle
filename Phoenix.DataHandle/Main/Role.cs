a7dbe4c90c36abbe2da8227bd1008f01281e13c4
Talagozis <talagozis8@gmail.com>
2020-06-13 19:44:29 +0300

    Added explicit enumeration in Role enum.

--- Phoenix.DataHandle/Main/Role.cs
+++ Phoenix.DataHandle/Main/Role.cs
@@ -1,15 +1,51 @@
-﻿namespace Phoenix.DataHandle.Main
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Phoenix.DataHandle.Main
 {
     public enum Role
     {
         Undefined = -1,
-        None,
-        Student,
-        Parent,
-        Secretary,
-        Teacher,
-        Admin,
-        SchoolOwner,
-        SuperAdmin,
+        None = 0,
+        Student = 1,
+        Parent = 2,
+        Secretary = 3,
+        Teacher = 4,
+        Admin = 5,
+        SchoolOwner = 6,
+        SuperAdmin = 7,
     }
+
+    public static class RoleExtensions
+    {
+        public static IEnumerable<Role> GetAll()
+        {
+            return Enum.GetValues(typeof(Role)).Cast<Role>();
+        }
+
+        public static string ToString(this Role me)
+        {
+            return me.ToString();
+        }
+
+        public static string ToStringNormalize(this Role me)
+        {
+            return me.ToString().ToUpper();
+        }
+
+        public static Role ToRole(this string me)
+        {
+            try
+            {
+                return GetAll().SingleOrDefault(a => string.Equals(a.ToString(), me, StringComparison.CurrentCultureIgnoreCase));
+            }
+            catch
+            {
+                return Role.Undefined;
+            }
+        }
+
+    }
+
 }
