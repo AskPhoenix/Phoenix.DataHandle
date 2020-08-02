dd4a608c23728dee8f0441b559e0e27356389140
Théophile Spyrou <alfatheo21@gmail.com>
2020-08-02 13:31:10 +0300

    Create script for packaging

---
+++ Phoenix.DataHandle/NuGet_Publish.cmd
@@ -0,0 +1,18 @@
+@echo off
+
+echo If you haven't authenticated GitHub stream, then use the command below:
+echo dotnet nuget add source https://nuget.pkg.github.com/AskPhoenix/index.json -n github -u AskPhoenix -p GH_TOKEN
+echo.
+echo.
+
+echo Packing for Release configuration...
+echo.
+
+dotnet pack --configuration Release
+
+echo.
+echo.
+set /p p="Enter version for package to publish: "
+dotnet nuget push "bin/Release/Phoenix.DataHandle.%p%.nupkg" --source "github"
+
+Pause
