1a6de7dc8c6bb0ef845f3b3ff33808b1fd891aa8
Théophile Spyrou <alfatheo21@gmail.com>
2020-11-12 23:20:59 +0200

    Update NuGet package publishing

--- Phoenix.DataHandle/NuGet_Publish.cmd
+++ Phoenix.DataHandle/NuGet_Publish.cmd
@@ -5,14 +5,18 @@ echo dotnet nuget add source https://nuget.pkg.github.com/AskPhoenix/index.json 
 echo.
 echo.
 
+echo Installing dependencies
+echo.
+
+dotnet restore
+
 echo Packing for Release configuration...
 echo.
 
-dotnet pack --configuration Release
+dotnet pack --configuration Release --no-restore
 
 echo.
 echo.
-set /p p="Enter version for package to publish: "
-dotnet nuget push "bin/Release/Phoenix.DataHandle.%p%.nupkg" --source "AskPhoenix"
+nuget.exe push "**/*.nupkg" -Source "AskPhoenix" -SkipDuplicate
 
 Pause
