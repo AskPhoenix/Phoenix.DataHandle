a46b8c385e051d48163ece6f6ff0120d91fa6098
Théophile Spyrou <alfatheo21@gmail.com>
2020-11-14 14:52:51 +0200

    Create namespace Phoenix.DataHandle.Utilities

---
+++ Phoenix.DataHandle/Utilities/CalendarExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Phoenix.DataHandle.Utilities
+{
+    public static class CalendarExtensions
+    {
+        public static int GetWeekOfYearISO8601(DateTime date)
+        {
+            var day = (int)CultureInfo.CurrentCulture.Calendar.GetDayOfWeek(date);
+            return CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(date.AddDays(4 - (day == 0 ? 7 : day)), CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+        }
+
+        public static int GetWeekOfYearISO8601(DateTimeOffset date) => GetWeekOfYearISO8601(date.DateTime);
+
+        public static DateTimeOffset GetDateTimeOffsetFromString(string dateTime, string format, IFormatProvider provider, TimeZoneInfo timeZone)
+        {
+            DateTime d = DateTime.ParseExact(dateTime, format, provider);
+            return new DateTimeOffset(d, timeZone.GetUtcOffset(d));
+        }
+
+        public static DateTimeOffset GetDateTimeOffsetFromString(string dateTime, string format)
+            => GetDateTimeOffsetFromString(dateTime, format, new CultureInfo("el-GR"), TimeZoneInfo.FindSystemTimeZoneById("E. Europe Standard Time"));
+    }
+}
---
+++ Phoenix.DataHandle/Utilities/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Phoenix.DataHandle.Utilities
+{
+    public static class StringExtensions
+    {
+        public static string UpperToTitleCase(this string str)
+        {
+            string tore = new string(str);
+
+            bool allUpper = tore == tore.ToUpper();
+            if (!allUpper)
+                return tore;
+
+            var textInfo = CultureInfo.InvariantCulture.TextInfo;
+
+            tore = tore.ToLower();
+            tore = textInfo.ToTitleCase(tore).Replace("σ ", "ς ").Replace("σ-", "ς-");
+
+            if (tore.EndsWith('σ'))
+            {
+                char[] toreArr = tore.ToCharArray();
+                toreArr[^1] = 'ς';
+                tore = new string(toreArr);
+            }
+
+            return tore;
+        }
+    }
+}
