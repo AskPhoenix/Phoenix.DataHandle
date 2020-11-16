686b29ebc413ff2f76029665251f1c3237a16f0e
Théophile Spyrou <alfatheo21@gmail.com>
2020-11-16 20:25:13 +0200

    Create methods to link Books with Courses

--- Phoenix.DataHandle/Repositories/CourseRepository.cs
+++ Phoenix.DataHandle/Repositories/CourseRepository.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Phoenix.DataHandle.Main.Models;
 
 namespace Phoenix.DataHandle.Repositories
@@ -19,6 +21,29 @@ namespace Phoenix.DataHandle.Repositories
             tModel.UpdatedAt = DateTimeOffset.Now;
 
             return base.Update(tModel);
+        }
+
+        public Course Update(Course tModel, Course tModelFrom)
+        {
+            tModel.Name = tModelFrom.Name;
+            tModel.SubCourse = tModelFrom.SubCourse;
+            tModel.Group = tModelFrom.Group;
+            tModel.Level = tModelFrom.Level;
+            tModel.FirstDate = tModelFrom.FirstDate;
+            tModel.LastDate = tModelFrom.LastDate;
+            tModel.Info = tModelFrom.Info;
+
+            return this.Update(tModel);
+        }
+
+        public void LinkBook(Course tModel, Book book)
+        {
+            this.dbContext.Set<CourseBook>().Add(new CourseBook() { CourseId = tModel.Id, BookId = book.Id });
+        }
+
+        public void LinkBooks(Course tModel, IEnumerable<Book> books)
+        {
+            this.dbContext.Set<CourseBook>().AddRange(books.Select(b => new CourseBook() { CourseId = tModel.Id, BookId = b.Id }));
         }
     }
 }
