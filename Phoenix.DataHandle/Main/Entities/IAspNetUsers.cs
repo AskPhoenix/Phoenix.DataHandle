1b832b1d551487958ec203b24b1bfe1a04b561c8
Christos Talagozis <talagozis@hotmail.com>
2020-11-21 04:22:17 +0200

    Remove unnecessary properties from IAspNetUers interface.

--- Phoenix.DataHandle/Main/Entities/IAspNetUsers.cs
+++ Phoenix.DataHandle/Main/Entities/IAspNetUsers.cs
@@ -7,19 +7,19 @@ namespace Phoenix.DataHandle.Main.Entities
     public interface IAspNetUsers
     {
         string UserName { get; set; }
-        string NormalizedUserName { get; set; }
+        //string NormalizedUserName { get; set; }
         string Email { get; set; }
-        string NormalizedEmail { get; set; }
+        //string NormalizedEmail { get; set; }
         bool EmailConfirmed { get; set; }
-        string PasswordHash { get; set; }
-        string SecurityStamp { get; set; }
-        string ConcurrencyStamp { get; set; }
+        //string PasswordHash { get; set; }
+        //string SecurityStamp { get; set; }
+        //string ConcurrencyStamp { get; set; }
         string PhoneNumber { get; set; }
         bool PhoneNumberConfirmed { get; set; }
-        bool TwoFactorEnabled { get; set; }
-        DateTimeOffset? LockoutEnd { get; set; }
-        bool LockoutEnabled { get; set; }
-        int AccessFailedCount { get; set; }
+        //bool TwoFactorEnabled { get; set; }
+        //DateTimeOffset? LockoutEnd { get; set; }
+        //bool LockoutEnabled { get; set; }
+        //int AccessFailedCount { get; set; }
         DateTimeOffset RegisteredAt { get; }
         ApplicationType CreatedApplicationType { get; set; }
 
