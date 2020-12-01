7240612b1110892117e9f98964ea478c282e2fd7
Théophile Spyrou <alfatheo21@gmail.com>
2020-12-01 22:26:28 +0200

    Assign all Courses of the School to an Admin, School Owner or Super Admin

--- Phoenix.DataHandle/Services/SchoolUserService.cs
+++ Phoenix.DataHandle/Services/SchoolUserService.cs
@@ -8,6 +8,7 @@ using Phoenix.DataHandle.WordPress.Utilities;
 using Phoenix.DataHandle.WordPress.Wrappers;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Phoenix.DataHandle.Services
@@ -15,6 +16,7 @@ namespace Phoenix.DataHandle.Services
     public class SchoolUserService : WPService
     {
         private readonly AspNetUserRepository aspNetUserRepository;
+        private readonly SchoolRepository schoolRepository;
 
         protected override int CategoryId => PostCategoryWrapper.GetCategoryId(PostCategory.SchoolUser);
 
@@ -27,6 +29,7 @@ namespace Phoenix.DataHandle.Services
         {
             this.aspNetUserRepository = new AspNetUserRepository(phoenixContext);
             this.aspNetUserRepository.Include(u => u.User, u => u.UserSchool);
+            this.schoolRepository = new SchoolRepository(phoenixContext);
         }
 
         public override void DeleteComplement()
@@ -66,30 +69,38 @@ namespace Phoenix.DataHandle.Services
                     _logger.LogInformation($"Updating User for School User: {schoolUserPost.GetTitle()}");
                     aspNetUserRepository.Update(curAspNetUser, acfSchoolUser.ToContext(), acfSchoolUser.ExtractUser());
                 }
+                var finalUser = curAspNetUser ?? ctxAspNetUser;
 
                 //TODO: Delete old Roles if any
                 _logger.LogInformation($"Linking with the AspNetUserRoles of School User: {schoolUserPost.GetTitle()}");
                 
                 List<Role> rolesToAdd = new List<Role>(2);
-                if (!aspNetUserRepository.HasRole(curAspNetUser, acfSchoolUser.RoleType))
+                if (!aspNetUserRepository.HasRole(finalUser, acfSchoolUser.RoleType))
                     rolesToAdd.Add(acfSchoolUser.RoleType);
-                if (!aspNetUserRepository.HasRole(curAspNetUser ?? ctxAspNetUser, acfSchoolUser.SecondRoleType))
+                if (!aspNetUserRepository.HasRole(finalUser, acfSchoolUser.SecondRoleType))
                     rolesToAdd.Add(acfSchoolUser.SecondRoleType);
-                
-                aspNetUserRepository.LinkRoles(curAspNetUser ?? ctxAspNetUser, rolesToAdd);
+                aspNetUserRepository.LinkRoles(finalUser, rolesToAdd);
 
                 _logger.LogInformation($"Linking with the Courses of School User: {schoolUserPost.GetTitle()}");
 
-                short[] courseCodes = acfSchoolUser.ExtractCourseCodes();
-                List<int> userCourseIds = new List<int>(courseCodes.Length);
-                foreach (short courseCode in courseCodes)
+                List<int> userCourseIds;
+                if (rolesToAdd.Any(r => r >= Role.Admin))
                 {
-                    if (!this.TryGetCourseId(schoolId, courseCode, out int courseId))
-                        continue;
-                    userCourseIds.Add(courseId);
+                    userCourseIds = schoolRepository.FindCourses(schoolId).Select(c => c.Id).ToList();
+                }
+                else
+                {
+                    short[] courseCodes = acfSchoolUser.ExtractCourseCodes();
+                    userCourseIds = new List<int>(courseCodes.Length);
+                    foreach (short courseCode in courseCodes)
+                    {
+                        if (!this.TryGetCourseId(schoolId, courseCode, out int courseId))
+                            continue;
+                        userCourseIds.Add(courseId);
+                    }
                 }
 
-                aspNetUserRepository.LinkCourses(curAspNetUser ?? ctxAspNetUser, userCourseIds);
+                aspNetUserRepository.LinkCourses(finalUser, userCourseIds);
             }
 
             _logger.LogInformation("School Users synchronization finished");
