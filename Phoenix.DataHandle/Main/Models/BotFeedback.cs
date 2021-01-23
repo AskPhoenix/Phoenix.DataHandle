263441c6625cd5356f1aa194af6c645b033dfd35
Théophile Spyrou <alfatheo21@gmail.com>
2021-01-24 01:02:22 +0200

    Update BotFeedback Table

--- Phoenix.DataHandle/Main/Models/BotFeedback.cs
+++ Phoenix.DataHandle/Main/Models/BotFeedback.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
 namespace Phoenix.DataHandle.Main.Models
 {
     public partial class BotFeedback
     {
         public int Id { get; set; }
         public int AuthorId { get; set; }
-        public BotFeedbackOccasion Occasion { get; set; }
-        public BotFeedbackCategory? Category { get; set; }
+        public string TriggerActionName { get; set; }
+        public string Type { get; set; }
         public short? Rating { get; set; }
         public string Comment { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
