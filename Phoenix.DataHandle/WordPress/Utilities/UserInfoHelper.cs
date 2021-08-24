b628969cd08a7ea4e4eec71528db01e2dd7ebd0c
Théophile Spyrou <alfatheo21@gmail.com>
2021-08-24 16:28:38 +0300

    Add null checks in GetName methods

--- Phoenix.DataHandle/WordPress/Utilities/UserInfoHelper.cs
+++ Phoenix.DataHandle/WordPress/Utilities/UserInfoHelper.cs
@@ -7,13 +7,22 @@ namespace Phoenix.DataHandle.WordPress.Utilities
     {
         public static string GetFirstName(string fullname)
         {
+            if (string.IsNullOrEmpty(fullname))
+                throw new ArgumentNullException(nameof(fullname));
+
             var names = fullname.Split(' ', StringSplitOptions.RemoveEmptyEntries);
             return string.Join(' ', names.Take((int)Math.Ceiling(names.Length / 2.0)));
         }
 
         public static string GetLastName(string fullname)
         {
+            if (string.IsNullOrEmpty(fullname))
+                throw new ArgumentNullException(nameof(fullname));
+
             var names = fullname.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (names.Length == 1)
+                return string.Empty;
+
             return string.Join(' ', names.TakeLast(names.Length / 2));
         }
     }
