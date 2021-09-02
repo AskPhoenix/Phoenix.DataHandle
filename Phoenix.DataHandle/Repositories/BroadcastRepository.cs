cbbaeaa036aa9de70156345668ac46438e2e15bf
Théophile Spyrou <theofilos.spyrou@gmail.com>
2021-09-02 15:46:35 +0300

    Create Broadcast Repository

---
+++ Phoenix.DataHandle/Repositories/BroadcastRepository.cs
@@ -0,0 +1,9 @@
+﻿using Phoenix.DataHandle.Main.Models;
+
+namespace Phoenix.DataHandle.Repositories
+{
+    public class BroadcastRepository : Repository<Broadcast>
+    {
+        public BroadcastRepository(PhoenixContext dbContext) : base(dbContext) { }
+    }
+}
