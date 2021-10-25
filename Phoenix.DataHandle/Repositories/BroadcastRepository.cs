c29f0a2f3ba98e1de59737658bba414b92e82c19
Théophile Spyrou <theofilos.spyrou@gmail.com>
2021-10-25 16:55:07 +0200

    Find Broadcasts for a specific day and daypart

--- Phoenix.DataHandle/Repositories/BroadcastRepository.cs
+++ Phoenix.DataHandle/Repositories/BroadcastRepository.cs
@@ -1,9 +1,22 @@
-﻿using Phoenix.DataHandle.Main.Models;
+﻿using Phoenix.DataHandle.Main;
+using Phoenix.DataHandle.Main.Models;
+using System;
+using System.Linq;
 
 namespace Phoenix.DataHandle.Repositories
 {
     public class BroadcastRepository : Repository<Broadcast>
     {
         public BroadcastRepository(PhoenixContext dbContext) : base(dbContext) { }
+
+        public IQueryable<Broadcast> FindForDate(DateTime date)
+        {
+            return this.Find().Where(b => b.ScheduledDate.Date == date);
+        }
+
+        public IQueryable<Broadcast> FindForDateDaypart(DateTime date, Daypart daypart)
+        {
+            return this.FindForDate(date).Where(b => b.Daypart == daypart);
+        }
     }
 }
