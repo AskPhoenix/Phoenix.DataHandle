e41d163bf621c90e1f99bea9f28eaf52a56c3c65
Théophile Spyrou <theofilos.spyrou@gmail.com>
2022-01-09 00:56:04 +0200

    Make IObviableModelEntity interface public

--- Phoenix.DataHandle/Main/Models/Extensions/IObviableModelEntity.cs
+++ Phoenix.DataHandle/Main/Models/Extensions/IObviableModelEntity.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Phoenix.DataHandle.Main.Models.Extensions
 {
-    internal interface IObviableModelEntity : IModelEntity
+    public interface IObviableModelEntity : IModelEntity
     {
         bool IsObviated { get; set; }
         DateTimeOffset? ObviatedAt { get; set; }
