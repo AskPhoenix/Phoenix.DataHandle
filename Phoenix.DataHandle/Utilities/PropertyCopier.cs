deb06f807a6f141c4b13a3fcc1333ab523bdb326
Théophile Spyrou <theofilos.spyrou@gmail.com>
2022-04-23 02:59:34 +0300

    Create Property Copier Utility

---
+++ Phoenix.DataHandle/Utilities/PropertyCopier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Phoenix.DataHandle.Utilities
+{
+    public static class PropertyCopier
+    {
+        public static void CopyFromBase<TTo, TFrom>(TTo to, TFrom from)
+            where TTo : TFrom
+        {
+            if (to is null)
+                throw new ArgumentNullException(nameof(to));
+            if (from is null)
+                throw new ArgumentNullException(nameof(from));
+
+            var toProps = typeof(TTo).GetProperties();
+            var fromProps = typeof(TFrom).GetProperties();
+
+            PropertyInfo? toProp;
+            foreach (var fromProp in fromProps)
+            {
+                toProp = toProps.FirstOrDefault(
+                    p => p.Name == fromProp.Name && p.PropertyType == fromProp.PropertyType);
+                if (toProp is null || !toProp.CanWrite)
+                    continue;
+
+                toProp.SetValue(to, fromProp.GetValue(from));
+            }
+        }
+    }
+}
