f19a2e2f63f7a901b8c6c64f66d4449e878b7628
Théophile Spyrou <theofilos.spyrou@gmail.com>
2022-04-23 03:03:13 +0300

    Create Tense Type

---
+++ Phoenix.DataHandle/Main/Types/Tense.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Phoenix.DataHandle.Main.Types
+{
+    public enum Tense
+    {
+        Never = 0,
+        Anytime,
+        Past,
+        Future
+    }
+
+    public static class TenseExtensions
+    {
+        public static Tense ToTense(this string me)
+        {
+            return Enum.GetValues<Tense>()
+                    .SingleOrDefault(t => string.Equals(t.ToString(), me, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public static bool TryToTense(this string me, out Tense tense)
+        {
+            tense = me.ToTense();
+
+            return Enum.GetValues<Tense>()
+                .Any(t => string.Equals(t.ToString(), me, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
