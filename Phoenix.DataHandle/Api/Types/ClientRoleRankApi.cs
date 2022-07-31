8a6983ab71f33c393de5fc9d87db576d0cfed943
Théophile Spyrou <theofilos.spyrou@gmail.com>
2022-08-01 00:36:23 +0300

    Create Personnel and Client Role Rank Api types

---
+++ Phoenix.DataHandle/Api/Types/ClientRoleRankApi.cs
@@ -0,0 +1,10 @@
+﻿using Phoenix.DataHandle.Main.Types;
+
+namespace Phoenix.DataHandle.Api.Types
+{
+    public enum ClientRoleRankApi
+    {
+        Student = RoleRank.Student - RoleHierarchy.ClientRolesBase,
+        Parent = RoleRank.Parent - RoleHierarchy.ClientRolesBase
+    }
+}
---
+++ Phoenix.DataHandle/Api/Types/PersonnelRoleRankApi.cs
@@ -0,0 +1,12 @@
+﻿using Phoenix.DataHandle.Main.Types;
+
+namespace Phoenix.DataHandle.Api.Types
+{
+    public enum PersonnelRoleRankApi
+    {
+        Teacher = RoleRank.Teacher - RoleHierarchy.StaffRolesBase,
+        Secretary = RoleRank.Secretary - RoleHierarchy.StaffRolesBase,
+        SchoolAdmin = RoleRank.SchoolAdmin - RoleHierarchy.StaffRolesBase,
+        SchoolOwner = RoleRank.SchoolOwner - RoleHierarchy.StaffRolesBase
+    }
+}
