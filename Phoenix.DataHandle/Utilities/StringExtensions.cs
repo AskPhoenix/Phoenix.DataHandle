d64fbba99c5d7f66832da3eb048eecd871868b7d
Théophile Spyrou <theofilos.spyrou@gmail.com>
2022-08-13 18:16:16 +0300

    Move string Normalization methods from Bot Utilities

--- Phoenix.DataHandle/Utilities/StringExtensions.cs
+++ Phoenix.DataHandle/Utilities/StringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Text;
 
 namespace Phoenix.DataHandle.Utilities
 {
@@ -14,6 +15,25 @@ namespace Phoenix.DataHandle.Utilities
         public static string Truncate(this string str, int maxLength)
         {
             return str.Length <= maxLength ? str : str[..maxLength];
+        }
+
+        public static string ToUnaccented(this string me)
+        {
+            string fullCanonicalDecompositionNormalized = me.Normalize(NormalizationForm.FormD);
+            var unaccented = fullCanonicalDecompositionNormalized
+                .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark);
+
+            return new string(unaccented.ToArray());
+        }
+
+        public static string RemoveEmojis(this string me, bool postTrim = true)
+        {
+            string newMe = new(me.Where(c => !char.IsSurrogate(c) && !char.IsSymbol(c)).ToArray());
+
+            if (postTrim)
+                newMe = newMe.Trim();
+
+            return newMe;
         }
     }
 }
