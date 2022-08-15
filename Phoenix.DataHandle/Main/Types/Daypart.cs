c06c220ef48384ae1d9f074777497503becaa203
Théophile Spyrou <theofilos.spyrou@gmail.com>
2022-08-15 23:02:35 +0300

    Update Daypart Extensions

--- Phoenix.DataHandle/Main/Types/Daypart.cs
+++ Phoenix.DataHandle/Main/Types/Daypart.cs
@@ -6,14 +6,27 @@ namespace Phoenix.DataHandle.Main.Types
     {
         Never = 0,
         Now,
-        Morning,        // 09:00
-        Midday,         // 12:00
-        Afternoon,      // 17:00
-        Evening         // 20:00
+        Morning,
+        Midday,
+        Afternoon,
+        Evening
+    }
+
+    public static class DaypartStartingHours
+    {
+        public const int MorningStartingHour    = 9;
+        public const int MiddayStartingHour     = 12;
+        public const int AfternoonStartingHour  = 17;
+        public const int EveningStartingHour    = 20;
     }
 
     public static class DaypartExtensions
     {
+        public static bool IsSlotDaypart(this Daypart me) => me >= Daypart.Morning;
+
+        public static Daypart[] AllDayparts => Enum.GetValues<Daypart>();
+        public static Daypart[] SlotDayparts => AllDayparts.Where(dp => dp.IsSlotDaypart()).ToArray();
+
         public static string ToFriendlyString(this Daypart me)
         {
             return me switch
@@ -29,10 +42,29 @@ namespace Phoenix.DataHandle.Main.Types
 
         public static string[] GetFriendlyStrings()
         {
-            return Enum.GetValues<Daypart>()
+            return AllDayparts
                 .Select(v => v.ToFriendlyString())
                 .Where(s => !string.IsNullOrWhiteSpace(s))
                 .ToArray();
+        }
+
+        public static Daypart[] FindFutureDayparts(DateTimeOffset reference)
+        {
+            List<Daypart> parts = new(AllDayparts);
+            parts.Remove(Daypart.Never);
+
+            int hour = reference.Hour;
+            
+            if (hour > DaypartStartingHours.MorningStartingHour)
+                parts.Remove(Daypart.Morning);
+            if (hour > DaypartStartingHours.MiddayStartingHour)
+                parts.Remove(Daypart.Midday);
+            if (hour > DaypartStartingHours.AfternoonStartingHour)
+                parts.Remove(Daypart.Afternoon);
+            if (hour > DaypartStartingHours.EveningStartingHour)
+                parts.Remove(Daypart.Evening);
+
+            return parts.ToArray();
         }
     }
 }
